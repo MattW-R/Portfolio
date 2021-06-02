97748737b6546d5e5a20e99c4adcdf9b7b711d5b
MattW-R <Matthew.wr89@gmail.com>
2021-06-02 13:21:33 +0100

    Implemented source maps

--- gulpfile.js
+++ gulpfile.js
@@ -11,49 +11,33 @@ const sassCompile = () => {
     return gulp.src('app/scss/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('app/css'))
-}
-
-const cssAutoPrefix = () => {
-    return gulp.src('app/css/*.css')
         .pipe(autoprefixer({
             cascade: false
         }))
-        .pipe(gulp.dest('app/css'))
-}
-
-const cssCompress = () => {
-    return gulp.src('app/css/*.css')
         .pipe(cssnano())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('app/css'))
 }
 
 const tsCompile = () => {
     return tsProject.src()
+        .pipe(sourcemaps.init())
         .pipe(tsProject())
-        .js.pipe(gulp.dest('app/js'))
-}
-
-const jsCompress = () => {
-    return gulp.src('app/js/*.js')
         .pipe(minify({
             ext:{
                 min:'.js'
             },
             noSource: true
         }))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('app/js'))
 }
 
 const watch = () => {
-    gulp.watch('app/scss/**/*.scss', gulp.series(sassCompile, cssAutoPrefix, cssCompress))
-    gulp.watch('app/ts/*.ts', gulp.series(tsCompile, jsCompress))
+    gulp.watch('app/scss/**/*.scss', sassCompile)
+    gulp.watch('app/ts/*.ts', tsCompile)
 }
 
 exports.sassCompile = sassCompile
-exports.cssAutoPrefix = cssAutoPrefix
-exports.cssCompress = cssCompress
 exports.tsCompile = tsCompile
-exports.jsCompress = jsCompress
 exports.watch = watch
