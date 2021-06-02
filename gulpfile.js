396572f247dd096bd982202127b6cbe03ae61687
MattW-R <Matthew.wr89@gmail.com>
2021-06-02 16:52:49 +0100

    Added gulp build & build-dev command

--- gulpfile.js
+++ gulpfile.js
@@ -40,11 +40,41 @@ const tsCompile = () => {
         .pipe(gulp.dest('app/js'))
 }
 
-const watch = () => {
-    gulp.watch('app/scss/**/*.scss', sassCompile)
-    gulp.watch('app/ts/*.ts', tsCompile)
+const sassBuild = (cb) => {
+    gulp.src('app/scss/*.scss')
+        .pipe(size({title: 'scss: '}))
+        .pipe(sass())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(size({title: 'css: '}))
+        .pipe(cssnano())
+        .pipe(size({title: 'nano-css: '}))
+        .pipe(gulp.dest('app/css'))
+    cb()
+}
+
+const tsBuild = (cb) => {
+    tsProject.src()
+        .pipe(size({title: 'ts: '}))
+        .pipe(tsProject())
+        .pipe(size({title: 'js: '}))
+        .pipe(minify({
+            ext:{
+                min:'.js'
+            },
+            noSource: true
+        }))
+        .pipe(size({title: 'min-js: '}))
+        .pipe(gulp.dest('app/js'))
+    cb()
 }
 
 exports.sassCompile = sassCompile
 exports.tsCompile = tsCompile
-exports.watch = watch
+exports.watch = () => {
+    gulp.watch('app/scss/**/*.scss', sassCompile)
+    gulp.watch('app/ts/*.ts', tsCompile)
+}
+exports.buildDev = gulp.series(sassCompile, tsCompile)
+exports.build = gulp.series(sassBuild, tsBuild)
