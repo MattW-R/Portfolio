a230de643db9666ac15766c5d04214ea32490f36
MattW-R <Matthew.wr89@gmail.com>
2021-06-03 10:53:52 +0100

    Added html minification to gulp build & restructured compilation destination & gulp build functions

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp')
+const htmlmin = require('gulp-htmlmin')
 const sass = require('gulp-sass')
 const autoprefixer = require('gulp-autoprefixer')
 const cssnano = require('gulp-cssnano')
@@ -9,74 +10,84 @@ const minify = require('gulp-minify')
 const tsProject = ts.createProject('tsconfig.json')
 const size = require('gulp-size')
 
-const sassCompile = () => {
-    return gulp.src('app/scss/*.scss')
+const htmlBuild = (cb) => {
+    gulp.src('app/**/*.html')
+        .pipe(size({title: 'html:'}))
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(size({title: 'min-html:'}))
+        .pipe(gulp.dest('dist'))
+    return cb()
+}
+
+const sassBuildDev = (cb) => {
+    gulp.src('app/scss/*.scss')
         .pipe(sourcemaps.init())
-        .pipe(size({title: 'scss: '}))
+        .pipe(size({title: 'scss:'}))
         .pipe(sass())
         .pipe(autoprefixer({
             cascade: false
         }))
-        .pipe(size({title: 'css: '}))
+        .pipe(size({title: 'css:'}))
         .pipe(cssnano())
-        .pipe(size({title: 'nano-css: '}))
+        .pipe(size({title: 'nano-css:'}))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('app/css'))
+        .pipe(gulp.dest('dist/css'))
+    return cb()
 }
 
-const tsCompile = () => {
-    return tsProject.src()
+const tsBuildDev = (cb) => {
+    tsProject.src()
         .pipe(sourcemaps.init())
-        .pipe(size({title: 'ts: '}))
+        .pipe(size({title: 'ts:'}))
         .pipe(tsProject())
-        .pipe(size({title: 'js: '}))
+        .pipe(size({title: 'js:'}))
         .pipe(minify({
             ext:{
                 min:'.js'
             },
             noSource: true
         }))
-        .pipe(size({title: 'min-js: '}))
+        .pipe(size({title: 'min-js:'}))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('app/js'))
+        .pipe(gulp.dest('dist/js'))
+    return cb()
 }
 
 const sassBuild = (cb) => {
     gulp.src('app/scss/*.scss')
-        .pipe(size({title: 'scss: '}))
+        .pipe(size({title: 'scss:'}))
         .pipe(sass())
         .pipe(autoprefixer({
             cascade: false
         }))
-        .pipe(size({title: 'css: '}))
+        .pipe(size({title: 'css:'}))
         .pipe(cssnano())
-        .pipe(size({title: 'nano-css: '}))
-        .pipe(gulp.dest('app/css'))
+        .pipe(size({title: 'nano-css:'}))
+        .pipe(gulp.dest('dist/css'))
     cb()
 }
 
 const tsBuild = (cb) => {
     tsProject.src()
-        .pipe(size({title: 'ts: '}))
+        .pipe(size({title: 'ts:'}))
         .pipe(tsProject())
         .pipe(stripDebug())
-        .pipe(size({title: 'js: '}))
+        .pipe(size({title: 'js:'}))
         .pipe(minify({
             ext:{
                 min:'.js'
             },
             noSource: true
         }))
-        .pipe(size({title: 'min-js: '}))
-        .pipe(gulp.dest('app/js'))
+        .pipe(size({title: 'min-js:'}))
+        .pipe(gulp.dest('dist/js'))
     cb()
 }
 
-exports.sassCompile = sassCompile
-exports.tsCompile = tsCompile
 exports.watch = () => {
-    gulp.watch('app/scss/**/*.scss', sassCompile)
-    gulp.watch('app/ts/*.ts', tsCompile)
+    gulp.watch('app/**/*.html', htmlBuild)
+    gulp.watch('app/scss/*.scss', sassBuildDev)
+    gulp.watch('app/ts/**/*.ts', tsBuildDev)
 }
-exports.buildDev = gulp.series(sassCompile, tsCompile)
-exports.build = gulp.series(sassBuild, tsBuild)
+exports.buildDev = gulp.series(htmlBuild, sassBuildDev, tsBuildDev)
+exports.build = gulp.series(htmlBuild, sassBuild, tsBuild)
