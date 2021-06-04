e77e6c18a00b256f3e1d615bfbd6cc8ef015a468
MattW-R <Matthew.wr89@gmail.com>
2021-06-04 17:31:52 +0100

    Started functionality to control scrolling between sections.

--- app/ts/ScrollControl.ts
+++ app/ts/ScrollControl.ts
@@ -1,10 +1,38 @@
+const findNextScrollAnchor = (scrollDirection: 'up'|'down'): HTMLElement|null => {
+    const scrollAnchors: NodeListOf<HTMLElement> = document.querySelectorAll<HTMLElement>('.scroll-anchor')
+    if (scrollAnchors.length > 0) {
+        let closestAnchorIndex: number = 0
+        for (let i = 0; i < scrollAnchors.length; i++) {
+            if (Math.abs(scrollAnchors[closestAnchorIndex].getBoundingClientRect().top - window.scrollY) >= Math.abs(scrollAnchors[i].getBoundingClientRect().top - window.scrollY)) {
+                closestAnchorIndex = i
+            }
+        }
+        console.log(closestAnchorIndex)
+        if (scrollDirection === 'up' && closestAnchorIndex > 0) {
+            return scrollAnchors[closestAnchorIndex - 1]
+        } else if (scrollDirection === 'down' && closestAnchorIndex < scrollAnchors.length - 1) {
+            return scrollAnchors[closestAnchorIndex + 1]
+        } else {
+            return scrollAnchors[closestAnchorIndex]
+        }
+    } else {
+        return null
+    }
+}
+
 let oldScrollPosition = 0
 
-window.addEventListener('scroll', () => {
+window.addEventListener('wheel', () => {
     if (window.scrollY > oldScrollPosition) {
-        console.log('Scroll Down')
+        const nextScrollAnchor = findNextScrollAnchor('down')
+        if (nextScrollAnchor !== null) {
+            window.scrollTo(0, nextScrollAnchor.getBoundingClientRect().top)
+        }
     } else if (window.scrollY < oldScrollPosition) {
-        console.log('Scroll Up')
+        const nextScrollAnchor = findNextScrollAnchor('up')
+        if (nextScrollAnchor !== null) {
+            window.scrollTo(0, nextScrollAnchor.getBoundingClientRect().top)
+        }
     }
     oldScrollPosition = window.scrollY
 })
