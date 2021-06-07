84635f6605724d7a6d019b7402676e7e16258407
MattW-R <Matthew.wr89@gmail.com>
2021-06-07 11:02:41 +0100

    Finished functionality to control scrolling between sections.

--- app/ts/ScrollControl.ts
+++ app/ts/ScrollControl.ts
@@ -3,11 +3,10 @@ const findNextScrollAnchor = (scrollDirection: 'up'|'down'): HTMLElement|null =>
     if (scrollAnchors.length > 0) {
         let closestAnchorIndex: number = 0
         for (let i = 0; i < scrollAnchors.length; i++) {
-            if (Math.abs(scrollAnchors[closestAnchorIndex].getBoundingClientRect().top - window.scrollY) >= Math.abs(scrollAnchors[i].getBoundingClientRect().top - window.scrollY)) {
+            if (Math.abs(scrollAnchors[closestAnchorIndex].getBoundingClientRect().top) >= Math.abs(scrollAnchors[i].getBoundingClientRect().top)) {
                 closestAnchorIndex = i
             }
         }
-        console.log(closestAnchorIndex)
         if (scrollDirection === 'up' && closestAnchorIndex > 0) {
             return scrollAnchors[closestAnchorIndex - 1]
         } else if (scrollDirection === 'down' && closestAnchorIndex < scrollAnchors.length - 1) {
@@ -22,17 +21,20 @@ const findNextScrollAnchor = (scrollDirection: 'up'|'down'): HTMLElement|null =>
 
 let oldScrollPosition = 0
 
+window.addEventListener('scroll', () => {
+    oldScrollPosition = window.scrollY
+})
+
 window.addEventListener('wheel', () => {
     if (window.scrollY > oldScrollPosition) {
         const nextScrollAnchor = findNextScrollAnchor('down')
         if (nextScrollAnchor !== null) {
-            window.scrollTo(0, nextScrollAnchor.getBoundingClientRect().top)
+            window.scrollTo(0, - document.querySelector('body').getBoundingClientRect().top + nextScrollAnchor.getBoundingClientRect().top)
         }
     } else if (window.scrollY < oldScrollPosition) {
         const nextScrollAnchor = findNextScrollAnchor('up')
         if (nextScrollAnchor !== null) {
-            window.scrollTo(0, nextScrollAnchor.getBoundingClientRect().top)
+            window.scrollTo(0, - document.querySelector('body').getBoundingClientRect().top + nextScrollAnchor.getBoundingClientRect().top)
         }
     }
-    oldScrollPosition = window.scrollY
 })
